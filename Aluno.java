75af624bd39826c69f56f6b4c2db5aa0bf0049c0
renanmarx26 <101154828+renanmarx26@users.noreply.github.com>
2024-03-08 04:36:24 -0300

    Exercicio 2

--- Aluno.java
+++ Aluno.java
@@ -2,36 +2,11 @@ public class Aluno {
 
     String nome;
     int matricula;
-    double nota_mat;
-    double nota_por;
-    double nota_fis;
-    double nota_geo;
-    double nota_quim;
-    double media_nota;
 
-    public Aluno(String nome, int matricula, double nota_mat, double nota_por, double nota_fis, double nota_geo, double nota_quim) {
-        this.nome = nome;
-        this.matricula = matricula;
-        this.nota_mat = nota_mat;
-        this.nota_por = nota_por;
-        this.nota_fis = nota_fis;
-        this.nota_geo = nota_geo;
-        this.nota_quim = nota_quim;
-    }
+    Disciplina disciplinaMatriculada;
 
-    void calcularMedia(double media) {
-        media_nota = ((nota_mat + nota_por + nota_fis + nota_geo + nota_quim) / 5);
-    }
 
-    void situacao_Aluno(double nota_final) {
-        if (media_nota >= 6) {
-            System.out.println("Aprovado!");
-        }
-        else{
-            System.out.println("Reprovado!");
-            }
 
-    }
+
 }
-
 
--- Principal.java
+++ Principal.java
@@ -1,27 +1,29 @@
 public class Principal {
     public static void main(String[] args) {
 
-        Aluno a1 = new Aluno("Renan", 100, 6, 5, 8, 9, 7);
-        Aluno a2 = new Aluno("Marx", 200, 5, 6, 7, 1, 0);
+        Aluno a1 = new Aluno();
+        Aluno a2 = new Aluno();
+        Disciplina d1 = new Disciplina();
+        Disciplina d2 = new Disciplina();
 
-        System.out.println("\nNome: " +a1.nome+"\nMatricula: "+a1.matricula+"\nNota Mat: "+a1.nota_mat+"\nNota Port: "+a1.nota_por+"\nNota Fis: "+a1.nota_fis+"\nNota Geo: "+a1.nota_geo+"\nNota Quim: "+a1.nota_quim);
-        a1.nota_mat = 6;
-        a1.nota_por = 5;
-        a1.nota_fis = 8;
-        a1.nota_geo = 9;
-        a1.nota_quim = 7;
-        a1.calcularMedia((a1.media_nota = a1.nota_mat + a1.nota_por + a1.nota_fis + a1.nota_geo + a1.nota_geo + a1.nota_quim)/5);
-        a1.situacao_Aluno(a1.media_nota);
+        a1.nome = "Renan";
+        a1.matricula = 2013;
+        a1.disciplinaMatriculada = d1;
+
+        a2.nome = "Marx";
+        a2.matricula = 2021;
+        a2.disciplinaMatriculada = d2;
+
+        d1.nome = "POO";
+        d1.cargaHoraria = 120;
+
+        d2.nome = "Java";
+        d2.cargaHoraria = 90;
+
+        System.out.println("\nNome: " +a1.nome+"\nMatricula: "+a1.matricula+"\nNome Disciplina: "+a1.disciplinaMatriculada.nome+"\nCarga Horário: "+a1.disciplinaMatriculada.cargaHoraria);
+        System.out.println("\nNome: " +a2.nome+"\nMatricula: "+a2.matricula+"\nNome Disciplina: "+a2.disciplinaMatriculada.nome+"\nCarga Horário: "+a2.disciplinaMatriculada.cargaHoraria);
 
 
-        System.out.println("\nNome: " +a2.nome+"\nMatricula: "+a2.matricula+"\nNota Mat: "+a2.nota_mat+"\nNota Port: "+a2.nota_por+"\nNota Fis: "+a2.nota_fis+"\nNota Geo: "+a2.nota_geo+"\nNota Quim: "+a2.nota_quim);
-        a2.nota_mat = 5;
-        a2.nota_por = 6;
-        a2.nota_fis = 7;
-        a2.nota_geo = 1;
-        a2.nota_quim = 0;
-        a2.calcularMedia(a2.media_nota = ((a2.nota_mat + a2.nota_por + a2.nota_fis + a2.nota_geo + a2.nota_quim) / 5));
-        a2.situacao_Aluno(a2.media_nota);
 
     }
 }
